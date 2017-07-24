6c0e633b58c01017bb62e748853f02117310ca24
Guoxin.Liu <lgx31@sina.cn>
2017-07-24 15:01:02 +0800

    delete --eval

---
+++ uninstall-opensips.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+############################################################
+
+#         name : Install OpenSips
+
+#         author : liugx@bjnangle.com
+
+############################################################
+
+rm -rf /usr/local/etc/opensips/
+rm -rf /usr/local/share/opensips/
+rm -rf /usr/local/share/man/
+rm -rf /usr/local/sbin/
