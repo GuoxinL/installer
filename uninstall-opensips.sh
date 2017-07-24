200bd7ba63dfe43c89a0255f3b2edf2a795bf295
Guoxin.Liu <lgx31@sina.cn>
2017-07-24 15:53:29 +0800

    delete --eval

--- uninstall-opensips.sh
+++ uninstall-opensips.sh
@@ -12,4 +12,6 @@ rm -rf /usr/local/etc/opensips/
 rm -rf /usr/local/share/opensips/
 rm -rf /usr/local/share/man/
 rm -rf /usr/local/sbin/
+rm -rf /usr/local/lib64/opensips/
+rm -rf /usr/local/share/doc/opensips/
 find /usr -name "opensips*"
