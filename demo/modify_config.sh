32500f40b40783789a8162428151df3db544b3da
Guoxin.Liu <lgx31@sina.cn>
2017-07-25 11:02:12 +0800

    delete --eval

--- demo/modify_config.sh
+++ demo/modify_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source ../utils/smelly_and_long.sh
-addrs=/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"
+addrs=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 config_path="/usr/local/etc/opensips/opensips.cfg"
 mv ${config_path} ${config_path}.bak
 while read line
--- opensips.sh
+++ opensips.sh
@@ -50,7 +50,7 @@ source /etc/profile
 # 设置OpenSips 配置
 echo -e "$opensios_config_mysql" > /usr/local/etc/opensips/opensipsctlrc
 
-addrs=/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"
+addrs=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 config_path="/usr/local/etc/opensips/opensips.cfg"
 mv ${config_path} ${config_path}.bak
 while read line
@@ -63,6 +63,7 @@ do
         echo $line >> $config_path
     fi
 done
+
 ## 设置为服务
 #set_application_as_service opensips "$opensips_service_conf"
 #
