325179f3c34f24ceeb4818121de439caa3f46387
Guoxin.Liu <lgx31@sina.cn>
2017-07-27 10:00:11 +0800

    delete --eval

--- java.sh
+++ java.sh
@@ -22,13 +22,10 @@ check_system
 # su -
 echo "INSTALLING JAVA 8 AS USER `whoami` "
 
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
-
+add-apt-repository ppa:webupd8team/java
+apt-get update
 # Accept license non-iteractive
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-apt-get update
 apt-get install -y oracle-java8-installer
 
 # Make sure Java 8 becomes default java
